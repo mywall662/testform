3287a98f9428c9f02cfba30c0bb78da43038f770
mywall662 <62753520+mywall662@users.noreply.github.com>
2024-11-15 01:44:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var formData = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value
-    };
-    fetch('https://script.google.com/macros/s/AKfycbxjddmlH3SOixo-nIZje4qbxOlma5SBjevqpo4GBr_t3y0W6X0-4tX5z_JL99JC6oGdgg/exec', {
-        method: 'POST',
-        body: JSON.stringify(formData)
-    }).then(response => response.text())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
-});
+fetch('https://script.google.com/macros/s/AKfycbxjddmlH3SOixo-nIZje4qbxOlma5SBjevqpo4GBr_t3y0W6X0-4tX5z_JL99JC6oGdgg/exec', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(formData)
+})
+.then(response => response.text())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
